6f670a5a45cb4b42748bd24e3d040f1663e607d9
lud <ludovic@demblans.com>
2025-12-12 12:53:42 +0100

    (libcheck)

--- .libcheck.exs
+++ .libcheck.exs
@@ -2,6 +2,8 @@
   github_stale_issues: [
     enabled: true,
     days_before_issue_stale: 7,
-    days_before_issue_close: 7
+    days_before_issue_close: 7,
+    days_before_pr_stale: 14,
+    days_before_pr_close: 14
   ]
 ]
